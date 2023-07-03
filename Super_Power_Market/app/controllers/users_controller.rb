548cfe85df9359e7a835e367d79da89e5792217e
pedrofonsecapadilha <pedrofpadilha@gmail.com>
2023-07-03 11:17:17 -0300

    alterando local do projeto

--- Super_Power_Market/app/controllers/users_controller.rb
+++
@@ -1,7 +0,0 @@
-class UsersController < ApplicationController
-  def show
-    @user = User.find(params[:id])
-    @orders = Order.where(user_id: current_user)
-    @super_powers = SuperPower.where(user_id: current_user)
-  end
-end
--- Super_Power_Market/db/migrate/20221123165404_add_column_img_to_super_power.rb
+++
@@ -1,5 +0,0 @@
-class AddColumnImgToSuperPower < ActiveRecord::Migration[7.0]
-  def change
-    add_column :super_powers, :img, :text
-  end
-end
